1272248ac29deb945deae989f61914c8f6489ec9
julzedz <edoziechukwuma@gmail.com>
2023-07-14 18:01:27 +0100

    Add post count methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,17 @@ class Post < ApplicationRecord
   belongs_to :user, foreign_key: 'author_id'
   has_many :comments
   has_many :likes
+
+  after_create :increment_post_count
+  after_destroy :decrement_post_count
+
+  def increment_post_count
+    puts 'Incrementing post count'
+    user.increment!(:posts_counter)
+  end
+
+  def decrement_post_count
+    puts 'Decrementing post count'
+    user.decrement!(:posts_counter)
+  end
 end
